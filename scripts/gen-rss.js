f86ac5962832f49e2edd394709baa5481e8dc2ba
Minmoy <91714349+minmoydutta@users.noreply.github.com>
2023-10-25 17:10:27 +0530

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Minmoy Dutta',
+    site_url: 'https://minmoydutta.github.io/Blog-Template',
+    feed_url: 'https://minmoydutta.github.io/Blog-Template/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
