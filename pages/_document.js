3c397e6bc65f221b28c7df1a12cbddc430e058fc
Minmoy <91714349+minmoydutta@users.noreply.github.com>
2023-10-25 17:09:45 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Minmoy Dutta',
+    description: 'Minmoy Dutta Blog.',
+    image: 'https://minmoydutta.github.io/image/i.jpeg'
   }
 
   return (
